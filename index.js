a681fd19e6c32b4c31b9a7470d3a29bba0de2101
naomiinyele <inyeleinyele@gmail.com>
2023-09-27 11:24:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 
   let input; 
-  input = document.getElementById('button');
+ // input = document.getElementById('button');
 
  function addingEventListener() {
   alert('I was clicked!');
+  input = document.getElementById('button');
+  input.addEventListener('click', addingEventListener);
  }
- 
-input.addEventListener('click', addingEventListener);
+ addingEventListener()
+//input.addEventListener('click', addingEventListener);
 
 
 
